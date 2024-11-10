648b8a1389c4dbf939bc81babcc7a0ce80b4effd
Dimas Maendra <dimas.maendra@talenavi.com>
2024-11-10 12:00:21 +0700

    parallel stream test

--- src/test/java/dimas/belajar/stream/ParallelStreamTest.java
+++ src/test/java/dimas/belajar/stream/ParallelStreamTest.java
@@ -1,4 +1,30 @@
 package dimas.belajar.stream;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
 public class ParallelStreamTest {
+
+    @Test
+    void testSequential() {
+//      dijalankan di satu proses secara sequentsial
+        Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).forEach(number -> {
+
+            System.out.println(Thread.currentThread().getName() + " : " + number);
+
+        });
+
+    }
+
+    @Test
+    void testParallel() {
+//      dijalankan dengan banyak thread secara parallel menggunakan class fork join pool
+        Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).parallel().forEach(number -> {
+
+            System.out.println(Thread.currentThread().getName() + " : " + number);
+
+        });
+
+    }
 }
